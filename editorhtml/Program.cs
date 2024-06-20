f943cd2e8429941d73a70f0e210ccdf486c7152f
Andrieli Cheva <andrielicheva@employer.com.br>
2024-06-20 10:16:26 -0300

    Adicionar arquivos de projeto.

---
+++ editorhtml/Program.cs
@@ -0,0 +1,13 @@
+﻿using editorhtml;
+using System;
+
+namespace EditorHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
